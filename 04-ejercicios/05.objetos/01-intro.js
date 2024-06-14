95aca2f586bd6cd17e945aa767579d40947729cd
xXLONELYXx <22302025@utfv.edu.mx>
2024-06-13 22:32:14 -0500

    1
    1

---
+++ 04-ejercicios/05.objetos/01-intro.js
@@ -0,0 +1,16 @@
+
+let user = {
+    email : 'lonely@gmail.com',
+    name : 'LONELY',
+    direccion: {
+        calle: 'Queen st',
+        numero: 15,
+    },
+    activo : true,
+    recuperarClave : function () {
+        console.log('Recuperando Clave....');
+
+    }
+
+
+};
